f46f4598f20b077e97f3e41541287f787eab9ac3
BohdanBurkun <Fuck170398>
2022-08-15 13:43:53 +0300

    Created new.

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -1,32 +1,36 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class MainTests extends BaseUI {
-
-
     By registrationButton = By.cssSelector("button[id='show-registration-block']");
     By textFiledEmail = By.xpath("//input[@id='email']");
     By textFiledPassword = By.cssSelector("input[id='password']");
     By nextButton = By.xpath("//button[@data-action='next-page'][text()='Next']");
     By nickName = By.xpath("//input[@name='data[nickname]']");
-    By birthDateDropDownDay = By.xpath("//a[@data-toggle='dropdown']");
-    By birthDateDay = By.xpath("//li[@data-handler='selectDay']");
-    By birthDateDropDownMonth = By.xpath("//a[@data-toggle='dropdown']");
-    By birthDateMonth = By.xpath("//li[@data-handler='selectMonth']");
-    By birthDateDropDownYear = By.xpath("//a[@data-toggle='dropdown']");
-    By birthDateYear = By.xpath("//li[@data-handler='selectYear']");
+    By birthDateDropDownDay = By.xpath("//div[@id='daySelect']");
+    By birthDateDay = By.xpath("//ul[@class='dropdown-menu show']//a[@data-value='17']");
+    By birthDateDropDownMonth = By.xpath("//div[@id='monthSelect']");
+    By birthDateMonth = By.xpath("//ul[@class='dropdown-menu show']//a[text()='Mar']");
+    By birthDateDropDownYear = By.xpath("//div[@id='yearSelect']");
+    By birthDateYear = By.xpath("//ul[@class='dropdown-menu show']//a[text()='1998']");
     By phone = By.xpath("//input[@name='data[phone]']");
-    By region = By.xpath("//input[@name='region_name']");
+    By confirmationCheckBox = By.xpath("//input[@id='confirmation']");
+    By regionNameAutocomplete = By.xpath("//input[@name='region_name']");
+    By selectedCapitalOfUkraine = By.xpath("//li[text()='Lozova, Ukraine']");
+    By youtubeFrameLocator  = By.xpath("//iframe[@src='https://www.youtube.com/embed/RRECuJzm3IY?start=85']");
+    By playYoutubeButton = By.xpath("//button[@aria-label='Смотреть']");
+
 
 
     String email = "zzzzzz@gmail.com";
     String password = "123456";
     String textNickName = "Bohdan";
     String textPhone = "+380123456798";
-    String textRegion = "Lozova";
+    String city = "Lozova";
 
 
     @Test
@@ -34,19 +38,30 @@ public class MainTests extends BaseUI {
         driver.findElement(registrationButton).click();
         driver.findElement(textFiledEmail).sendKeys(email);
         driver.findElement(textFiledPassword).sendKeys(password);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(nextButton).click();
         driver.findElement(nickName).sendKeys(textNickName);
-        driver.findElements(birthDateDropDownDay).get(0).click();
-        driver.findElements(birthDateDay).get(1).click();
-        driver.findElements(birthDateDropDownMonth).get(1).click();
-        driver.findElements(birthDateMonth).get(3).click();
-        driver.findElements(birthDateDropDownYear).get(2).click();
-        driver.findElements(birthDateYear).get(7).click();
-            wait.until(ExpectedConditions.elementToBeClickable(phone));
+        driver.findElement(birthDateDropDownDay).click();
+        driver.findElement(birthDateDay).click();
+        driver.findElement(birthDateDropDownMonth).click();
+        driver.findElement(birthDateMonth).click();
+        driver.findElement(birthDateDropDownYear).click();
+        driver.findElement(birthDateYear).click();
         driver.findElement(phone).sendKeys(textPhone);
-        driver.findElement(region).sendKeys(textRegion);
+        driver.findElement(confirmationCheckBox).click();
+        driver.findElement(regionNameAutocomplete).clear();
+        driver.findElement(regionNameAutocomplete).sendKeys(city);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(selectedCapitalOfUkraine)));
+        driver.findElement(selectedCapitalOfUkraine).click();
 
+
+    }
+
+    @Test
+    public void testIframe(){
+        WebElement youtubeIframe = driver.findElement(youtubeFrameLocator);
+        driver.switchTo().frame(youtubeIframe);
+        driver.findElement(playYoutubeButton).click();
     }
 
 }
